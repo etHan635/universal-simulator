79ac2deeab1e29496912cbe0e66462fe0ec121a8
ethan <ehanley03@qub.ac.uk>
2021-11-06 17:10:03 +0000

    created simple test data for URL refactoring

--- dataStub.js
+++ dataStub.js
@@ -4,3 +4,37 @@ var data={
 		shortname:"Data"
 	},
 };
+
+data["Test"] = {
+	battery:{
+		name:"9V Battery",
+		voltage:9,
+		connectionPoints:{
+			"anode":{},
+			"cathode":{},
+		},
+	},
+	bulb:{
+		name:"25W Bulb",
+		power:25,
+		connectionPoints:{
+			"leg1":{},
+			"leg2":{},
+		}
+	},
+	wireA:{
+		colour:"red",
+		connects:{
+			point1:"Test/battery/connectionPoints/anode",
+			point2:"Test/bulb/connectionPoints/leg1",
+		}
+	},
+	wireB:{
+		colour:"black",
+		connects:{
+			point1:"",
+			point2:""
+		}
+	}
+
+}
