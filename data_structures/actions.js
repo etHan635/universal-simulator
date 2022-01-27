d8642ff3645c06da9aa52664e95c78f241400b19
ethan <ehanley03@qub.ac.uk>
2022-01-27 11:51:04 +0000

    reimplemented 'walk' action

---
+++ data_structures/actions.js
@@ -0,0 +1,30 @@
+data["actions"] = {
+	walk:{
+		_visualisation:{shortname:"Walk",},
+		duration:1.0,
+		params:{
+			thatLocation:{type:"pick", options:"@agent/_visible/locations/*"},
+			// thatPosition:{type:"pick", options:"@args/thatLocation/agents/*"},
+			motivationCost:{type:"readonly", value:1},
+		},
+		transforms:{
+			pre:[
+				"nAppend(\"@agent/resources/motivation/quantity\", -1, actionInstance)",
+				// or: "actionInstance.agent.resources.motivation.quantity -= 1",
+				//Storing starting location for later
+				"nSet(\"@args/thisLocation'\", nContents(actionInstance.agent.properties.location, actionInstance), actionInstance)",
+				// "console.log(nContents(actionInstance.args[\"thisLocation'\"]._parent, actionInstance))",
+				"nDeleteFromArray(actionInstance.agent.properties.location, actionInstance.agent.properties._parent, actionInstance)",
+				"nSet(\"@agent/properties/location\", \"@/Locations/traveling\", actionInstance)",
+				//Just storing the *actual* destination for easy reference
+				"nSet(\"@args/thatLocation'\", nContents(nContents(\"@args/thatLocation\", actionInstance), actionInstance), actionInstance)",
+				"nAppend(\"@args/thatLocation'/resources/space/quantity\", -1, actionInstance)"
+			],
+			post:[
+				"nSet(\"@agent/properties/location\", actionInstance.args[\"thatLocation'\"].resources._parent + \"/agents\", actionInstance)",
+				//TODO: add agent to location.agents array
+				"nAppend(\"@args/thisLocation'/../resources/space/quantity\", 1, actionInstance)"
+			]
+		}
+	}
+}
